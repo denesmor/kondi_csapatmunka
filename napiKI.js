581ac2f9ad013491aee7a668c09566a9684e703e
BigMocky <mbalhek@gmail.com>
2025-04-07 14:10:31 +0200

    a 24 órás js ciklus megírásának kezdése, próbálkozás a napiKI js oldalon

--- napiKI.js
+++ napiKI.js
@@ -7,23 +7,61 @@ let napiKi = {
     _3: "dik"
 
 }
+
+function gererate(){
+    let randomNum = Math.round((Math.random() * 4)); // 0 to 3
+    console.log(randomNum);
+   // let elem = napiKi[randomNum]
+    return randomNum
+}
+
+let ujra = true
+let szamok = []
+let telilista = false
 function timer_passage(){
-    var now = new Date()
-  
+
+    while (telilista)
+    {
+        while (ujra)
+        {
+            let randomNum = gererate()
         
-        let randomNum = Math.round((Math.random() * 4)); // 0 to 3
-        console.log(randomNum);
-        let elem = napiKi[randomNum]
-        
-  
+            let ugyanaz = false
+            for (let i = 0; i < szamok.length; i++)
+            {
+                if (randomNum == szamok[i])
+                {
+                    ugyanaz = true
+                }
+            }
+
+            if (ugyanaz == true)
+            {
+                ujra = false
+            }
+            console.log('------------');
+
+            for (let i = 0; i < szamok.length; i++)
+            {
+                console.log(szamok[i]);
+            }
+            console.log('------------');
+
+            if (szamok.length = 4)
+            {
+                telilista = true
+            }
+        }
+    }
+    
         document.getElementById("demo").innerHTML = elem;
          
         //do something you want
 
     
-    //setTimeout(timer_passage, 600)//exec myself after 60000 ms
+    setTimeout(gererate, 6000)//exec myself after 60000 ms
+
+    
 }
 
 timer_passage();
-
-
