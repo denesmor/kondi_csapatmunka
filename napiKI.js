893b2b31d82696dfcd45d8645a76bbae159d1b1f
BigMocky <mbalhek@gmail.com>
2025-04-09 11:21:35 +0200

    NapiKI js elkezdese

--- napiKI.js
+++ napiKI.js
@@ -4,64 +4,49 @@ let napiKi = {
     _0: "asdasd",
     _1: "dasd",
     _2: "jey",
-    _3: "dik"
+    _3: "dik",
+    _4: "more",
+    _5: "dikhtv",
+    _6: "jokytv",
+    _7: "joslas"
 
 }
 
 function gererate(){
-    let randomNum = Math.round((Math.random() * 4)); // 0 to 3
+
+    let randomNum = Math.round((Math.random() * 8)); 
     console.log(randomNum);
-   // let elem = napiKi[randomNum]
+
     return randomNum
+    
+    
 }
+
 
 let ujra = true
 let szamok = []
 let telilista = false
+let elem;
 function timer_passage(){
 
-    while (telilista)
-    {
-        while (ujra)
-        {
-            let randomNum = gererate()
-        
-            let ugyanaz = false
-            for (let i = 0; i < szamok.length; i++)
-            {
-                if (randomNum == szamok[i])
-                {
-                    ugyanaz = true
-                }
-            }
+    let randomNum = gererate()
+            
 
-            if (ugyanaz == true)
-            {
-                ujra = false
-            }
-            console.log('------------');
+            elem = napiKi[randomNum]
+            
 
-            for (let i = 0; i < szamok.length; i++)
-            {
-                console.log(szamok[i]);
-            }
-            console.log('------------');
 
-            if (szamok.length = 4)
-            {
-                telilista = true
-            }
-        }
-    }
+            setTimeout(timer_passage, 6000)
+         }
+
     
         document.getElementById("demo").innerHTML = elem;
          
-        //do something you want
+
 
     
-    setTimeout(gererate, 6000)//exec myself after 60000 ms
 
+        
     
-}
 
 timer_passage();
